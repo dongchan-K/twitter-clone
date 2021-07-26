4e082f576cb7480315de6e8c0c3cd1c7e1eaf6d9
dongchan-K <ehdcks1028@gmail.com>
2021-07-26 13:22:52 +0900

    conf: eslit 및 prettier 설정

--- front/components/Layout.jsx
+++ front/components/Layout.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <div>공통 메뉴</div>
+      <div>
+        <Link href='/'>
+          <a>노드버드</a>
+        </Link>
+        <Link href='/profile'>
+          <a>프로필</a>
+        </Link>
+        <Link href='/signup'>
+          <a>회원가입</a>
+        </Link>
+      </div>
       {children}
     </div>
   );
 };
 
+// children 요소의 타입 지정
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 };
